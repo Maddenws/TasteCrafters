0ea816ee69756be3b78672973a3e2893f1632b74
Shane Madden <Madrox251@aol.com>
2024-05-29 17:59:03 -0400

    Added Saved Recipe Page

--- TasteCrafters/TasteCrafters/AppShell.xaml.cs
+++ TasteCrafters/TasteCrafters/AppShell.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using TasteCrafters.ViewModels;
 using TasteCrafters.Views;
 using Xamarin.Forms;
@@ -19,6 +20,31 @@ namespace TasteCrafters
         private async void OnMenuItemClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//LoginPage");
+        }
+
+        protected override async void OnNavigating(ShellNavigatingEventArgs args)
+        {
+            base.OnNavigating(args);
+
+            // Check if navigating to the SavedRecipesPage
+            if (args.Target.Location.OriginalString.Contains("SavedRecipePage"))
+            {
+
+                args.Cancel();
+
+                // Remove SavedRecipesPage
+                var navigationStack = Shell.Current.Navigation.NavigationStack.ToList();
+                foreach (var page in navigationStack)
+                {
+                    if (page is SavedRecipesPage)
+                    {
+                        Shell.Current.Navigation.RemovePage(page);
+                    }
+                }
+
+                // Push a new instance of SavedRecipesPage
+                await Shell.Current.Navigation.PushAsync(new SavedRecipesPage());
+            }
         }
     }
 }
